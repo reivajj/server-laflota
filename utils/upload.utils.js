c4b088f1ff86249efa492763c574419c5477fd00
reivajj <javierpetri2012@gmail.com>
2022-05-03 20:04:08 -0300

    Cambio el chunk size a MB y saco el -1 LPM

--- utils/upload.utils.js
+++ utils/upload.utils.js
@@ -2,52 +2,12 @@ const FormData = require('form-data');
 
 const getEndOfChunk = (index, chunksize, totalParts, totalSize) => {
   if (index + 1 === totalParts) return totalSize - 1;
-  else return (index + 1) * chunksize - 1;
+  else return (index + 1) * chunksize;
 }
-
-// REVEER: Pasar a FOR no con PROMISES... Chequear que no se pierde tanta eficacia.
-// Realmente dudo que haya un problema aca. El RETRY deberia poder solucionar algo.
-// const uploadFileByChunks = async (file, coverUploadStartUuid, mimeType, fileExtension, fileName, uploadFunction) => {
-//   let chunksize = 2000000;
-//   let totalParts = parseInt(file.size / chunksize) + 1;
-//   let arrayChunks = [...Array(totalParts).keys()];
-//   console.time("test upload");
-
-//   for (chunkIndex of arrayChunks) {
-//     let cutBufferAt = getEndOfChunk(chunkIndex, chunksize, totalParts, file.size);
-//     let startBuffer = chunkIndex * chunksize;
-
-//     let chunkFile = {
-//       fieldname: fileName,
-//       originalname: `${fileName}-${chunkIndex}.${fileExtension}`,
-//       mimetype: mimeType,
-//       buffer: file.buffer.slice(startBuffer, cutBufferAt)
-//     }
-
-//     const formDataFile = new FormData();
-//     formDataFile.append("uuid", coverUploadStartUuid);
-//     formDataFile.append("filename", file.originalname);
-//     formDataFile.append("totalfilesize", file.size);
-//     formDataFile.append("partindex", chunkIndex);
-//     formDataFile.append("chunksize", chunksize);
-//     formDataFile.append("partbyteoffset", chunksize * chunkIndex);
-//     formDataFile.append("totalparts", totalParts);
-//     formDataFile.append("file", chunkFile.buffer, chunkFile.originalname);
-
-//     await uploadFunction(formDataFile).catch(error => {
-//       console.log("ERROR: ", error);
-//       return error;
-//     });
-//     console.log("CHUNK INDEX: ", chunkIndex)
-//   }
-
-//   console.timeEnd("test upload");
-// }
-
 
 const uploadFileByChunks = async (file, coverUploadStartUuid, mimeType, fileExtension, fileName, uploadFunction) => {
   let oneMB = 1048576;  
-  let chunksize = fileExtension === "jpg" ? 6 * oneMB : 2 * oneMB;
+  let chunksize = 2 * oneMB;
   let totalParts = parseInt(file.size / chunksize) + 1;
   let arrayChunks = [...Array(totalParts).keys()];
   console.time("test upload");
@@ -59,8 +19,9 @@ const uploadFileByChunks = async (file, coverUploadStartUuid, mimeType, fileExte
       fieldname: fileName,
       originalname: `${fileName}-${chunkIndex}.${fileExtension}`,
       mimetype: mimeType,
-      buffer: file.buffer.slice(startBuffer, cutBufferAt)
+      buffer: file.buffer.slice(parseInt(startBuffer), parseInt(cutBufferAt))
     }
+    console.log(parseInt(startBuffer), parseInt(cutBufferAt), file.size);
 
     const formDataFile = new FormData();
     formDataFile.append("uuid", coverUploadStartUuid);
