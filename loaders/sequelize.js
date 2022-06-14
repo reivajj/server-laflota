b80797d54eda3d0c53223583585c40f8d4ce82c5
reivajj <javierpetri2012@gmail.com>
2022-06-13 21:57:48 -0300

    Flexibilizo royalties

--- loaders/sequelize.js
+++ loaders/sequelize.js
@@ -3,7 +3,7 @@ const { Sequelize } = require('sequelize');
 const Logger = require('./logger');
 const initModels = require("../sequelize/init-models");
 
-const sequelize = new Sequelize(config.mySqlDb.testUrl, {
+const sequelize = new Sequelize(config.mySqlDb.url, {
   logging: msg => Logger.info(msg),
 });
 console.log('Connection has been established successfully.');
